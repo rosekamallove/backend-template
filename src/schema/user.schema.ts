b0c45248f20c8d2983b9a183c4cdf416e8efd9df
Rose Kamal Love <rosekamallove@gmail.com>
2022-09-23 19:23:39 +0530

    updated schema

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -2,18 +2,19 @@ import { object, string, TypeOf } from "zod";
 
 export const createUserSchema = object({
   body: object({
-    firstName: string({
-      required_error: "First Name is required",
-    }),
-    lastName: string({
-      required_error: "Last Name is required",
-    }),
+    firstName: string().min(1, "First Name is required"),
+
+    lastName: string().min(1, "Last Name is required"),
+
     password: string({
-      required_error: "Password is required",
-    }).min(6, "Password too short - should be 6 chars minimum"),
-    passwordConfirmation: string({
-      required_error: "passwordConfirmation is required",
-    }),
+      required_error: "Name is required",
+    }).regex(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+      "Password (> 8 chars): uppercase, lowercase and special characthers"
+    ),
+
+    passwordConfirmation: string().min(1, "Please confirm passwords"),
+
     email: string({
       required_error: "Email is required",
     }).email("Not a valid email"),
